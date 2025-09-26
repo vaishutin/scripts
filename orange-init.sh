57d791e16871fa1356cf120734491620f0a3eae1
keraban <keraban@mayflower.work>
2025-09-27 00:01:33 +0300

    init

---
+++ orange-init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -f /etc/apt/sources.list.d/docker.list
+
+sudo tee /etc/apt/sources.list >/dev/null <<'EOF'
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb-src http://deb.debian.org/debian bullseye main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
+deb http://security.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://security.debian.org/debian-security/ bullseye-security main contrib non-free
+EOF
+
+apt-get update
