97c679a7485268b969f7f58cf3c8baff14144260
keraban <keraban@mayflower.work>
2025-09-27 00:52:24 +0300

    fix

--- orange-init.sh
+++ orange-init.sh
@@ -31,7 +31,7 @@ sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config &
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/010-nopasswd-$USER
 
 # Update hostname
-read -p "Введите номер для имени контроллера (rpiXXX): " num; echo
+read -p "Введите номер для имени контроллера (rpiXXX): " num
 
 sudo hostnamectl set-hostname "rpi${num}"
 echo "Имя хоста изменено на: rpi${num}"
